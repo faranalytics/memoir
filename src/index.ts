78d72b7aaa23fe6253b029f05670e69cffad0150
adpatter <adpatter@umich.edu>
2023-09-16 09:15:24 -0400

    Export MetadataFormatter.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export { Logger, Handler, Formatter, LoggerOptions, Level } from './abstract.js';
-export { RotatingFileHandler, RotatingFileHandlerOptions } from './rotating_file_handler.js';
 export { Metadata, MetadataOptions } from './metadata.js';
-export { LevelLogger, LevelLoggerOptions } from './level_logger.js';
-export { MetadataLogger } from './metadata_logger.js';
-export { ConsoleHandler } from './console_handler.js';
-export { LevelHandler } from './level_handler.js';
 export { MetadataFormatter } from './metadata_formatter.js';
+export { LevelHandler } from './level_handler.js';
+export { MetadataHandler } from './metadata_handler.js';
+export { ConsoleHandler } from './console_handler.js';
+export { RotatingFileHandler, RotatingFileHandlerOptions } from './rotating_file_handler.js';
+export { MetadataLogger } from './metadata_logger.js';
+export { LevelLogger, LevelLoggerOptions } from './level_logger.js';
