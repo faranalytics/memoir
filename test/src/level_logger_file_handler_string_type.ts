e1ef2b774886a3aa6a775f04b2b8f967d3057445
adpatter <adpatter@umich.edu>
2023-09-18 17:25:04 -0400

    Improve test.

--- test/src/level_logger_file_handler_string_type.ts
+++ test/src/level_logger_file_handler_string_type.ts
@@ -1,16 +1,19 @@
-import { LevelLogger, MetadataFormatter, Level, Metadata, RotatingFileHandler } from 'memoir';
+import { LevelLogger, MetadataFormatter, Level, Metadata, RotatingFileHandler, ConsoleHandler } from 'memoir';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const formatter = (message: string, { name, level, func, url, line, col }: Metadata): string =>
     `${name}:${level}:${new Date().toISOString()}:${func}:${line}:${col}:${message}`;
 
 const log = new LevelLogger<string, string>({ name: 'Rotating File Handler Example' }); // Create an instance of a Logger.
+const consoleHandler = new ConsoleHandler<string, string>(); // Create an instance of a Handler.
 const fileHandler = new RotatingFileHandler({ path: './test.log', rotations: 5 }); // Create an instance of a Handler.
 const metadataFormatter = new MetadataFormatter<string, string>({ formatter }); // Create an instance of a Formatter.
 
 fileHandler.setLevel(Level.DEBUG); // Set the Level of the Handler.
 fileHandler.setFormatter(metadataFormatter); // Set the Formatter on the Handler.
+consoleHandler.setFormatter(metadataFormatter); // Set the Formatter on the Handler.
 log.addHandler(fileHandler); // Add the Handler to the Logger.
+log.addHandler(consoleHandler); // Add the Handler to the Logger.
 
 log.info?.('Hello World.'); // Log a Hello World to the console.
 (function test() { log.info?.('Hello World.'); }());
